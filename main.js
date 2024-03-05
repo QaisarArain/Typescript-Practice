ca2059814e4ca0d7d1885785454e1e53bdeff2b9
QaisarArain <160647472+QaisarArain@users.noreply.github.com>
2024-03-05 10:05:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//camelCase
+//snake_case
+//PasacalCase
+var userName = 'Qaisar'; //e.g camelCase
+var user_name = 'Arain'; //e.g snake_case
+var UserName = 'Nauman Afzal'; // e.g PascalCase
+var isUserExit = true;
+var is_user_exit = false;
+var IsUserExit = 1;
+// = is assignment operator
+console.log(userName, user_name, UserName);
+console.log(isUserExit, is_user_exit, IsUserExit);
---
+++ main.ts
@@ -0,0 +1,18 @@
+//camelCase
+//snake_case
+//PasacalCase
+
+let userName = 'Qaisar' //e.g camelCase
+let user_name = 'Arain' //e.g snake_case
+let UserName = 'Nauman Afzal' // e.g PascalCase
+
+let isUserExit = true
+let is_user_exit = false
+let IsUserExit = 1;
+
+// = is assignment operator
+
+
+console.log(userName,user_name,UserName)
+console.log(isUserExit,is_user_exit,IsUserExit);
+
